7d4861cc76be1f7359e64d8eba7023e9a05803f3
Feras Abdulazem <feras.abdulazem@holzweg.com>
2022-11-10 17:30:12 +0100

    add the shopify api client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Shopify\Context;
+use Shopify\Auth\FileSessionStorage;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::unguard();
+
+        Context::initialize(
+            $_ENV['SHOPIFY_API_KEY'],
+            $_ENV['SHOPIFY_API_SECRET'],
+            $_ENV['SHOPIFY_APP_SCOPES'],
+            $_ENV['SHOPIFY_APP_HOST_NAME'],
+            new FileSessionStorage('/tmp/php_sessions'),
+            $_ENV['SHOPIFY_API_VERSION'],
+            true,
+            false,
+        );
     }
 }
