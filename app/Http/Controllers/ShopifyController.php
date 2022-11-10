57bc070f74f5066b9b24a860976dcd7259c382e3
Feras Abdulazem <feras.abdulazem@holzweg.com>
2022-11-10 17:31:44 +0100

    add shopify controller and route it

---
+++ app/Http/Controllers/ShopifyController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Service\ShopifyService;
+use Illuminate\Support\Facades\Log;
+
+class ShopifyController extends Controller
+{
+
+    private ShopifyService $shopifyService;
+
+
+    public function __construct(ShopifyService $shopifyService)
+    {
+        $this->shopifyService = $shopifyService;
+    }
+
+    public function storeOrder(Request $request)
+    {
+        Log::info("/api/shopify/orders");
+
+        $data = [
+            "name" => $request->name,
+            "email" => $request->email,
+            "tags" => $request->tags,
+        ];
+
+        $success = $this->shopifyService->storeOrder($data);
+
+
+        Log::info($success ? "successfully added order to shopify" : "failed to add the order to shopify");
+
+        return response()->json(compact("success"));
+    }
+}
