5727bd0b88daff81d1d5179f35b4b4dfde97d363
Feras Abdulazem <feras.abdulazem@holzweg.com>
2022-11-11 10:58:23 +0100

    style orders

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,25 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="{{asset('normalize.css')}}">
-    <link rel="stylesheet" href="{{asset('sakura.css')}}">
+    <link rel="stylesheet" href="{{ asset('normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('sakura.css') }}">
 
     <title>Orders</title>
 </head>
+
 <body>
-    
-    <ul>
-        @foreach($orders as $order)
-        <li>
-            {{$order->name}}
-        </li>
-        @endforeach
-    </ul>
-
-
+    <div>
+        <h1>Orders</h1>
+        <div style="display:flex; justify-content: flex-end"><a style="font-size: 30px" href="/orders/create">+</a>
+        </div>
+        <table>
+            <thead>
+                <th>
+                    name
+                </th>
+                <th>
+                    email
+                </th>
+                <th>
+                    Total Price
+                </th>
+            </thead>
+            <tbody>
+                @foreach ($orders as $order)
+                    <tr>
+                        <td>
+                            {{ $order->name }}
+                        </td>
+                        <td>
+                            {{ $order->email }}
+                        </td>
+                        <td>
+                            {{ $order->total_price }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>
