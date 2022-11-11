53bcd50266eb9cc0a38ba1ce2f47a92c1ac6fdc7
Feras Abdulazem <feras.abdulazem@holzweg.com>
2022-11-11 02:11:44 +0100

    make fields required

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -4,22 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <link rel="stylesheet" href="{{asset('normalize.css')}}">
+    <link rel="stylesheet" href="{{asset('sakura.css')}}">
+
+    <title>Creaete Order</title>
 </head>
 <body>
     <form method="POST" action="/orders" >
         @csrf
         <label for="name">Name</label>
-        <input name="name" id="name" />
+        <input required name="name" id="name" />
         
         <label for="email">email</label>
-        <input type="email" name="email" id="email" />
+        <input required type="email" name="email" id="email" />
 
         <label for="tags">tags</label>
-        <input type="string" name="tags" id="tags" />
+        <input required type="string" name="tags" id="tags" />
         
         <label for="total_price">Total Price</label>
-        <input type="number" name="total_price" id="total_price" />
+        <input required type="number" name="total_price" id="total_price" />
 
         <input type="submit" name="submit" />    
     </form>
